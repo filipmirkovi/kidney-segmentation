513d0afcf65b00ba947240f374fad9f102c74077
mirksonius <mirksonius@pop-os.home>
2025-02-22 18:02:17 +0100

    logging

--- train_model.py
+++ train_model.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import torch
 import mlflow
+from loguru import logger
+
 from torch.utils.data import DataLoader, random_split
 
 from src.dataset.SegmentationDataset import SegemetationDataset
@@ -17,14 +19,29 @@ random_seed = 100
 experiment = "Example-Run"
 device = "cuda:0"
 save_dir = "./app_data/model/"
+mlflow_tracking_uri = "http://10.100.111.210:44555"  # "http://127.0.0.1:44555"
 
-mlflow.set_tracking_uri(uri="http://127.0.0.1:44555")
+
+mlflow.set_tracking_uri(uri=mlflow_tracking_uri)
+
+# Configure loguru at the start of the script
+logger.add(
+    "training.log",  # Log file path
+    rotation="100 MB",  # Rotate file when it reaches 100MB
+    level="INFO",  # Log level
+    format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}",  # Log format
+)
+import sys
+
+logger.add(
+    sys.stdout, format="{time} {level} {message}", filter="my_module", level="INFO"
+)
 
 
 def main():
     mlflow.set_experiment(experiment)
     # torch.random.seed(random_seed)
-
+    logger.info("Setting up datasets...")
     train_set, validation_set = random_split(
         SegemetationDataset(
             images_path=Path(data_path, "train"),
@@ -34,22 +51,29 @@ def main():
     )
     train_dataloader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
     val_dataloader = DataLoader(validation_set, batch_size=batch_size, shuffle=False)
+    logger.info("Datasets created!...")
 
+    logger.info("Initializing model...")
     model: torch.nn.Module = UNet(
         in_channels=3, num_classes=3, hidden_channels=[32, 64, 128, 256]
     )
+    logger.info(f"Model built!\nSending model to device {device}")
     model = model.to(device)
     optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
     loss = torch.nn.BCEWithLogitsLoss(reduction="none")
+    logger.info("Initializing trainer...")
     trainer = Trainer(num_epochs=num_epochs, loss_fn=loss, device=device)
+    logger.info("Trainer initialzed!")
 
     with mlflow.start_run() as run:
+        logger.info(f"Starting run {run.info.run_name}\nBeginning training...")
         trainer.train(
             model,
             train_loader=train_dataloader,
             val_loader=val_dataloader,
             optimizer=optimizer,
         )
+    logger.info("Training done!")
 
 
 if __name__ == "__main__":
