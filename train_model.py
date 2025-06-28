0196a3dddb8b2f0449a8e2e11305ccb39f4395b5
mirksonius <filip.mirkoivc@atmc.ai>
2025-06-28 12:58:38 +0200

    fixed image size bug

--- train_model.py
+++ train_model.py
@@ -87,7 +87,7 @@ def main(config_path: str | Path):
     )
 
     model = model.to(configs["device"])
-    print(model(train_set[0][0][None]).shape)
+    print(model(train_set[0][0][None].to(configs["device"])).shape)
     optimizer = torch.optim.Adam(
         model.parameters(),
         lr=float(configs["lr"]),
