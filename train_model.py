9e04f022838d220c66aff6acad4904d642fbd47d
mirksonius <filip.mirkoivc@atmc.ai>
2025-07-12 15:38:21 +0200

    progress bar fix

--- train_model.py
+++ train_model.py
@@ -102,7 +102,7 @@ def main(config_path: str | Path):
         out_channels=4,
         hidden_size=128,
         num_perceptions=1024,
-        attenton_hidden_size=32,
+        attenton_hidden_size=64,
         num_scaling_layers=2,
         num_perciever_steps=12,
     )
